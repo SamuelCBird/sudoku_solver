a7408c019c09781a1d1764edd197566fe6d21f77
SBird <samuel@samuelcbird.co.uk>
2021-03-28 11:31:25 +0100

    abstracting code

--- public/classes/solvePuzzle.js
+++ public/classes/solvePuzzle.js
@@ -5,6 +5,69 @@ export class SolvePuzzle {
         this.startSolution();
     }
     ;
+    checkHorizontalValidity(value, index) {
+        console.log('HORIZ');
+        let i = index;
+        if (i < 9) {
+            i = 0;
+        }
+        else {
+            while (i % 9 != 0) {
+                i -= 1;
+            }
+        }
+        let slice = this.puzzle.slice(i, i + 9);
+        for (let j = 0; j < slice.length; j++) {
+            if (value != slice[j][0]) {
+                if (value.innerText === slice[j][0].innerText) {
+                    // console.log(`${value.innerText} / ${elem[0].innerText}`)
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    checkVerticalValidity(value, index) {
+        console.log('VERT');
+        let i = index;
+        if (i > 8) {
+            while (i - 9 >= 0) {
+                i -= 9;
+            }
+        }
+        for (i; i <= this.puzzle.length - 1; i += 9) {
+            if (value != this.puzzle[i][0]) {
+                if (value.innerText === this.puzzle[i][0].innerText) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    checkBlockValidity(value, index) {
+        console.log('BLOCK');
+        const blockCornerNumbers = [0, 3, 6, 27, 30, 33, 54, 57, 60];
+        let targetBlock;
+        // get the right block of 9
+        for (let j = 0; j < blockCornerNumbers.length; j++) {
+            for (let k = blockCornerNumbers[j]; k < (blockCornerNumbers[j] + 20); k + 9) {
+                for (let m = k; m < k + 3; m++) {
+                    if (index === m) {
+                        targetBlock = blockCornerNumbers[j];
+                    }
+                }
+            }
+        }
+        // check for number
+        for (let k = targetBlock; k < (k + 21); k + 9) {
+            for (let m = k; m < k + 3; m++) {
+                if (parseInt(value.innerText) === m) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
     startSolution() {
         let i = 0;
         while (i < this.puzzle.length) {
@@ -24,60 +87,32 @@ export class SolvePuzzle {
         }
     }
     isValid(value, index) {
-        // check horizontally
-        let i = index;
-        if (i < 9) {
-            i = 0;
-        }
-        else {
-            while (i % 9 != 0) {
-                i -= 1;
-            }
-        }
-        let slice = this.puzzle.slice(i, i + 9);
-        for (let j = 0; j < slice.length; j++) {
-            if (value != slice[j][0]) {
-                if (value.innerText === slice[j][0].innerText) {
-                    // console.log(`${value.innerText} / ${elem[0].innerText}`)
-                    return false;
+        if (this.checkHorizontalValidity(value, index)) {
+            if (this.checkVerticalValidity(value, index)) {
+                if (this.checkBlockValidity(value, index)) {
+                    return true;
                 }
             }
         }
-        // check vertically
-        i = index;
-        if (i > 8) {
-            while (i - 9 >= 0) {
-                i -= 9;
-            }
-        }
-        for (i; i <= this.puzzle.length - 1; i += 9) {
-            if (value != this.puzzle[i][0]) {
-                if (value.innerText === this.puzzle[i][0].innerText) {
-                    return false;
-                }
-            }
-        }
-        return true;
+        return false;
     }
     insertNumber(square, index) {
-        for (let i = 1; i < 10; i++) {
-            let newNumber;
-            if (!square.innerText) {
-                newNumber = 0;
-            }
-            else {
-                newNumber = parseInt(square.innerText);
-            }
-            if (newNumber < 9) {
-                newNumber += 1;
-            }
-            else {
-                return false;
-            }
-            square.innerText = newNumber.toString();
-            if (this.isValid(square, index)) {
-                return true;
-            }
+        let newNumber;
+        if (!square.innerText) {
+            newNumber = 0;
+        }
+        else {
+            newNumber = parseInt(square.innerText);
+        }
+        if (newNumber < 9) {
+            newNumber += 1;
+        }
+        else {
+            return false;
+        }
+        square.innerText = newNumber.toString();
+        if (this.isValid(square, index)) {
+            return true;
         }
         return false;
     }
